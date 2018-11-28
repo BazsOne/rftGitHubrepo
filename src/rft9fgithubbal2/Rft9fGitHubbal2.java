37778c98f7379883fe61cc6765e4fff9a3598ae3
Nagy Balazs <User@DESKTOP-26MA6R4>
2018-11-28 01:02:22 +0100

    asd

--- src/rft9fgithubbal2/Rft9fGitHubbal2.java
+++ src/rft9fgithubbal2/Rft9fGitHubbal2.java
@@ -15,7 +15,7 @@ public class Rft9fGitHubbal2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("asd");
     }
     
 }
